d1829338917f13200ddbf0c57302c6c78cd166bf
sndsabin <sndsabin@gmail.com>
2026-09-08 15:51:50 +0300

    bump version

--- main.go
+++ main.go
@@ -22,7 +22,7 @@ import (
 
 const (
 	AppName         = "pal"
-	AppVersion      = "0.0.4"
+	AppVersion      = "0.0.5"
 	AppDescription  = "A system tray app for staying in sync with moments across the world."
 	windowShowEvent = "window:show"
 	windowHideEvent = "window:hide"
